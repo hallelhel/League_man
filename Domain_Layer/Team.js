d48ba278a7d4c64d394565e763d11bafcce4f9e0
hallelhel <hallelh@post.bgu.ac.il>
2021-05-20 15:17:08 +0300

    service layer

---
+++ Domain_Layer/Team.js
@@ -0,0 +1,3 @@
+function Team(name){
+    this.name = name
+}
---
+++ Test/addTeam.test.js
@@ -0,0 +1,14 @@
+const team = require('League_man/Domain_Layer/System');
+
+test('....',()=>{
+    expect()
+})
+
+
+/* example:
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+*/
