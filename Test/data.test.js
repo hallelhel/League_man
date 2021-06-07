1286c8dadbd6750456a5d3465b57abea7e9fd4f7
noam <kotlar@post.bgu.ac.il>
2021-06-07 17:41:20 +0300

    data test

--- Test/data.test.js
+++ Test/data.test.js
@@ -6,15 +6,3 @@ test('col hakvod!!!!!', () =>{
 });
 
 
-async function getFromTable(tableName, lookUpValuesArray, constraint=null){//constraint =[username=noam, password=1243]
-    let valString ="";
-    lookUpValuesArray.forEach(x=> valString = valString.concat(x.toString(),', '));
-    let addOns = '';
-    if (constraint[0]){
-        addOns = ' WHERE '
-        constraint.forEach(x=> addOns.concat(x, ' AND '));
-        addOns = addOns.slice(0,5);
-    }
-    const dbQuery = `SELECT '${valString}' FROM '${tableName}'`.concat(addOns);
-    return await DButils.execQuery(dbQuery);
-}
