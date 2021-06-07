399395f40cf256dd5a762320ae08d8d05915ea45
noam <kotlar@post.bgu.ac.il>
2021-06-07 12:45:56 +0300

    test

--- Test/UserTest.js
+++ Test/UserTest.js
@@ -1,19 +1,27 @@
 var axios = require("axios");
-const expect = require("chai").expect
+// const expect = require("chai").expect
 let chaiHttp = require("chai-http");
 const chaiAsPromise = require("chai-as-promised");
 let chai = require('chai');
 const api_domain = "http://localhost:3000";
 
 //Log In tests
-describe('/POST login', () => {
-    it('OK, Correct username and password', async () => {
-        chai.request(`${api_domain}`)
-            .post('/login')
-            .send({username: 'admin', password: 'admin123'})
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.should.have.property('text').eql('Login succeeded')
-            });
-    });
+// describe('/POST login', () => {
+//     it('OK, Correct username and password', async () => {
+//         chai.request(`${api_domain}`)
+//             .post('/login')
+//             .send({username: 'admin', password: 'admin123'})
+//             .end((err, res) => {
+//                 res.should.have.status(200);
+//                 res.should.have.property('text').eql('Login succeeded')
+//             });
+//     });
+// });
+
+test('check log in', async()=>{
+    const response = await request(auth).post('/login').send({
+        username: 'admin',
+        password: 'admin123'
+    })
+    expect((response.statusCode).toBe(200))
 });
