6fea2274ec4082cf1fe8fb82586d1b9ecb49aff8
hallelhel <hallelh@post.bgu.ac.il>
2021-06-09 17:25:12 +0300

    test coverage

--- Test/addGame.test.js
+++ Test/addGame.test.js
@@ -10,13 +10,81 @@ const path = require("path")
 const DButils = require(path.join(__dirname, '../','Domail','../Data_Layer/DButils'));
 const bcrypt = require("bcryptjs");
 
+// describe('/Post games/LeagueManagment/addGame', ()=>{
+//     it('should return status code 200', (done)=>{
+//         var admin = chai.request.admin(`${api_domain}`)
+//         admin.post('/login')
+//         .send({username: "admin", password: "admin123"})
+//         .end((err,res)=> {
+//             admin.post('/games/LeagueManagment/addGame')
+//             .send({date: "2024-1-31",
+//                     hour: "22:30:00",
+//                     away_team_id: 939,
+//                     home_team_id: 1020,
+//                     field: "test",
+//                     referee_username: "noam"
+//                       })
+//             .end((err,res)=> {
+//                 res.should.have.status(200)
+//                 res.should.have.property('text').eql('Game added successfuly');
+//                 done();
+//             })
+//         }) 
+//     })
+//     after(async function(){
+//         await DButils.execQuery(`DELETE FROM games WHERE field='test'`);
+//     })
+// })
+//     // before(async function(){
+//     //     const resout= await chai.request(`${api_domain}`)
+//     //     .post('/user/logOut');
+//     //     const reslog= await chai.request(`${api_domain}`)
+//     //     .post('/login')
+//     //     .send({username: "admin", password: "admin123"})
+//     // })
+//     // context('test', function(){
+//     //     it('should return status code 200', async function(){
+//     //         const res = await chai.request(`${api_domain}`)
+//     //         .post('/games/LeagueManagment/addGame')
+//     //         .send({date: "2024-1-31",
+//     //         hour: "22:30:00",
+//     //         away_team_id: 939,
+//     //         home_team_id: 1020,
+//     //         field: "test",
+//     //         referee_username: "noam"
+//     //           })
+//     //         expect(res.status).to.equal(200)
+//     //         expect(res.text).to.equal('Game added successfuly');
+//     //     }) 
+//     // })
+// // // describe('/Post LeagueManagment/addGame', function() {
+// // //     context('with vaild details', function(){
+// // //         it('should return status code 400', async function(){
+// // //             const res =await chai.request(`${api_domain}`)
+// // //             .post('LeagueManagment/addGame')
+// // //             .send({date: "2022-11-31", hour: "20:30:00",
+// // //                 away_team_id: 100,
+// // //                 home_team_id: 1020,
+// // //                 field: "Tedi",
+// // //                 referee_username: "noam"
+// // //               })
+// // //               expect(res.status).to.equal(400);
+// // //               expect(res.text).to.equal('One or both team details are incorrect created');
+// // //         }) 
+// // //     })
+// // //     after(async function(){
+// // //         await DButils.execQuery("DELETE FROM games WHERE date='2022-10-31'");
+// // //     })
+// // // })
+
 describe('/Post games/LeagueManagment/addGame', ()=>{
     it('should return status code 200', (done)=>{
-        var admin = chai.request.admin(`${api_domain}`)
-        admin.post('/login')
+        var agent = chai.request.agent(`${api_domain}`)
+        agent.post('/login')
         .send({username: "admin", password: "admin123"})
         .end((err,res)=> {
-            admin.post('/games/LeagueManagment/addGame')
+            console.log(res.status)
+            agent.post('/games/LeagueManagment/addGame')
             .send({date: "2024-1-31",
                     hour: "22:30:00",
                     away_team_id: 939,
@@ -25,54 +93,12 @@ describe('/Post games/LeagueManagment/addGame', ()=>{
                     referee_username: "noam"
                       })
             .end((err,res)=> {
-                res.should.have.status(200)
+                console.log(res.status)
+                // res.should.have.status(200)
+                res.status.should.be.eql(200)
                 res.should.have.property('text').eql('Game added successfuly');
                 done();
             })
         }) 
     })
-    after(async function(){
-        await DButils.execQuery(`DELETE FROM games WHERE field='test'`);
-    })
 })
-    // before(async function(){
-    //     const resout= await chai.request(`${api_domain}`)
-    //     .post('/user/logOut');
-    //     const reslog= await chai.request(`${api_domain}`)
-    //     .post('/login')
-    //     .send({username: "admin", password: "admin123"})
-    // })
-    // context('test', function(){
-    //     it('should return status code 200', async function(){
-    //         const res = await chai.request(`${api_domain}`)
-    //         .post('/games/LeagueManagment/addGame')
-    //         .send({date: "2024-1-31",
-    //         hour: "22:30:00",
-    //         away_team_id: 939,
-    //         home_team_id: 1020,
-    //         field: "test",
-    //         referee_username: "noam"
-    //           })
-    //         expect(res.status).to.equal(200)
-    //         expect(res.text).to.equal('Game added successfuly');
-    //     }) 
-    // })
-// // describe('/Post LeagueManagment/addGame', function() {
-// //     context('with vaild details', function(){
-// //         it('should return status code 400', async function(){
-// //             const res =await chai.request(`${api_domain}`)
-// //             .post('LeagueManagment/addGame')
-// //             .send({date: "2022-11-31", hour: "20:30:00",
-// //                 away_team_id: 100,
-// //                 home_team_id: 1020,
-// //                 field: "Tedi",
-// //                 referee_username: "noam"
-// //               })
-// //               expect(res.status).to.equal(400);
-// //               expect(res.text).to.equal('One or both team details are incorrect created');
-// //         }) 
-// //     })
-// //     after(async function(){
-// //         await DButils.execQuery("DELETE FROM games WHERE date='2022-10-31'");
-// //     })
-// // })
